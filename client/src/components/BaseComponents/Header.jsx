d116005e9854071d5a7a9384f0b7382f53e5d493
melissa-24 <mlongenberger@codingdojo.com>
2026-04-16 20:03:17 -0400

    saving updated nav new nav not deployed

--- client/src/components/BaseComponents/Header.jsx
+++ client/src/components/BaseComponents/Header.jsx
@@ -1,12 +1,10 @@
 import './baseComponents.css'
 import Shelf from '../../assets/images/shelf.png'
-import { useNavigate } from 'react-router-dom'
 import useAuthStore from '../../store/authStore'
 import useLogout from '../../hooks/useLogout'
 
 
 export default function Header() {
-    const navigate = useNavigate()
     const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
     const { logout } = useLogout()
 
@@ -23,17 +21,19 @@ export default function Header() {
                 <a href="#">For Readers</a>
                 <a href="#">For Authors</a>
                 <a href="#">Shop</a>
-            </nav>
-            <div className="auth-buttons">
                 {isAuthenticated ? (
                     <>
-                        <button onClick={() => navigate('/dashboard')}>Dashboard</button>
-                        <button onClick={logout}>Logout</button>
+                        <a href='/dashboard'>Dashboard</a>
+                        <a onClick={logout} href="#">Logout</a>
                     </>
                 ) : (
-                    <button onClick={() => navigate('/signup')}>Sign Up</button>
+                    <>
+                        <a href='/login'>LogIn</a>
+                        <a href='/signup'>Sign Up</a>
+                    </>
                 )}
-            </div>
+            </nav>
+            
         </header>
         </>
     )
