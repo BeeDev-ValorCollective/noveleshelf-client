e115b32d6876344dffc8fad81342cc49bfb0067f
Christopher J <chrisbjones1987@yahoo.com>
2026-05-19 20:11:17 -0500

    Added freeAuthorDashboard.css to Dashboard components

--- client/src/views/Dashboards/FreeAuthorDashboard.jsx
+++ client/src/views/Dashboards/FreeAuthorDashboard.jsx
@@ -1,10 +1,19 @@
 import ProfileHeader from "../../components/DashboardComponents/ProfileHeader";
+import useUser from '../../hooks/useUser'
+import useFullName from '../../hooks/useFullName'
+import useAuthStore from '../../store/authStore'
 
 export default function FreeAuthorDashboard() {
 
+    const { user } = useUser()
+    const currentProfile = useAuthStore((state) => state.currentProfile)
+    const currentRole = useAuthStore((state) => state.currentRole)
+    const fullName = useFullName()
+
+
     return(
         <div className="free-author-dashboard-container">
-            <ProfileHeader />
+            <ProfileHeader user={user} currentProfile={currentProfile} fullName={fullName} currentRole={currentRole} />
         </div>
     )
 }
