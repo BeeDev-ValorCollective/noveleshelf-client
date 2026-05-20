20542b43c8a32741ce37dd372f4f80021d2a0df7
Christopher J <chrisbjones1987@yahoo.com>
2026-05-20 11:59:17 -0500

    Added FreeAuthorProjects component to FreeAuthorDashboard

--- client/src/views/Dashboards/FreeAuthorDashboard.jsx
+++ client/src/views/Dashboards/FreeAuthorDashboard.jsx
@@ -1,5 +1,6 @@
 import ProfileHeader from "../../components/DashboardComponents/ProfileHeader";
 import FreeAuthorProjects from "../../components/DashboardComponents/FreeAuthorDashboardComponents/FreeAuthorProjects";
+import StatsBar from "../../components/DashboardComponents/FreeAuthorDashboardComponents/StatsBar";
 import useUser from '../../hooks/useUser'
 import useFullName from '../../hooks/useFullName'
 import useAuthStore from '../../store/authStore'
@@ -15,6 +16,7 @@ export default function FreeAuthorDashboard() {
     return(
         <div className="free-author-dashboard-container">
             <ProfileHeader user={user} currentProfile={currentProfile} fullName={fullName} currentRole={currentRole} />
+            <StatsBar />
             <FreeAuthorProjects />
         </div>
     )
